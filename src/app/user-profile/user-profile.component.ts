97a5d76d06b70fd9492f5f38bc52ff4910ca04c1
balajiking23 <balajie2301@gmail.com>
2023-04-28 08:33:16 +0530

    filter class

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, takeUntil, takeWhile, timer } from 'rxjs';
 import { ApiService } from 'src/service/api.service';
 
 @Component({
@@ -6,52 +7,90 @@ import { ApiService } from 'src/service/api.service';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent {
 
 
 
   public numList: any;
 
+  public name = ""
 
+  public profile_id: any;
 
-  id: any;
-  name: any;
-  avatar: any;
-  createdAt: any;
+  public buttonclicked = 0;
+  public id = "";
+  public createdAt = "";
 
   constructor(private api: ApiService) { }
 
   getuserData() {
+
     this.api.fetchData().subscribe(res => {
-      console.log(res);
+      
       this.numList = res
       console.log(this.numList);
+
+      let windowUrl = new URL(window.location.href);
+
+      this.profile_id = windowUrl.searchParams.get("profile_id") ? windowUrl.searchParams.get("profile_id") : 0
+
+
+      if (this.profile_id > 0) {
+        this.buttonclicked = 1
+      }
+      for (let i = 0; i < this.numList.length; i++) {
+ 
+       if (this.profile_id == this.numList[i].id) {
+          this.name = this.numList[i].name;
+          this.id = this.numList[i].id;
+          this.createdAt = this.numList[i].createdAt;
+
+
+        }
+      }
 
     });
   }
 
   public now = new Date();
 
-  ngOnInit() {
-    console.log(this.now);
-    console.log(this.now.toLocaleDateString());
-    console.log(this.now.toLocaleString());
-    console.log(this.now.toUTCString());
-    console.log(this.now.toISOString());
-    this.now.setDate( this.now.getDate() + 3 );
-    console.log(this.now);
+  // ngOnInit() {
+  //   console.log(this.now);
+  //   console.log(this.now.toLocaleDateString());
+  //   console.log(this.now.toLocaleString());
+  //   console.log(this.now.toUTCString());
+  //   console.log(this.now.toISOString());
+  //   this.now.setDate( this.now.getDate() + 3 );
+  //   console.log(this.now);
 
-    let windowUrl = new URL(window.location.href);
 
-    let profile_id = windowUrl.searchParams.get("profile_id") ? windowUrl.searchParams.get("profile_id") :0
-     console.log(profile_id);
+  //    setTimeout(() => {
 
-     setTimeout(() => {
+  //     console.log("delay");
 
-      console.log("delay");
-      
-     }, 30000);
-    
-  }
+  //    }, 3000);
+
+  //    let count=0;
+  //    const timer$ = timer(30000);
+
+  //    let Delay:any;
+
+  //    interval(5000).pipe(takeWhile( () => (count<=6 )), takeUntil(timer$) )
+  //   .subscribe(() => {
+  //     count = count + 1
+  //     console.log(count);
+  //     if (count == 5)
+  //     {
+  //       clearInterval(Delay);
+
+  //     }
+
+
+
+  //   });
+
+  // }
+
+
 
 }
